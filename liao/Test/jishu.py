1aa21b17401a9722a4a89b738eb573f08961ddc4
josesky <josesky@live.cn>
2020-04-29 14:54:21 +0800

    delete many files

--- liao/Test/jishu.py
+++
@@ -1,3 +0,0 @@
-# print('My name is %s. I am %d  years old' % ('Tianxia',8))
-s = input('Enter a line: ')
-print('The number of words in the line are %d' % (len(s.split(" "))))
--- liao/Test/tuple.py
+++
@@ -1,7 +0,0 @@
-L = ('tianxia','tianwai',['A',"b"])
-print(L)
-L[2][0] = 'love'
-L[2][1] = 'make'
-
-print(L)
-
